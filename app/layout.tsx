7ad002b3878026bf9f06eb90033063059df109fb
yazanedali <edaliyazan6@gmail.com>
2025-07-30 00:26:23 +0300

    fixed deployment error

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-        <ClerkProvider>
+         <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
 
     <html lang="en" suppressHydrationWarning>
       <body
