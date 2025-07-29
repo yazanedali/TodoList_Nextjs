ddc5d516600f4c605adfbc2f589e3b1243db6079
yazanedali <edaliyazan6@gmail.com>
2025-07-30 00:36:56 +0300

    auth by middlewere

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const isProtectedRoute = createRouteMatcher(['/']) 
 
 
-// export default clerkMiddleware(async (auth, req) => {
-//   if (isProtectedRoute(req)) {
-//     await auth.protect() 
-//   }
-// })
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect() 
+  }
+})
 
-export default clerkMiddleware();
+
 
 export const config = {
   matcher: [
